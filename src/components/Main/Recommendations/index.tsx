e64d6741223225411ef7fb000a554392ea5361e5
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-08-18 03:05:48 +0900

    refactor: remove neon effect for better lcp

--- src/components/Main/Recommendations/index.tsx
+++ src/components/Main/Recommendations/index.tsx
@@ -1,36 +1,25 @@
-import { PAGE } from '@/constants/url';
 import { useRecommendations } from '@/hooks/query/useRecommendations';
 
 import AnimatedTitle from '@/components/_shared/AnimatedTitle';
+import { PAGE } from '@/constants/url';
 import NothingToShow from '../_shared/NothingToShow/NothingToShow';
 import Table from './Table';
 
 export default function Recommendations() {
   const { data, isLoading } = useRecommendations();
 
-  if (isLoading) {
-    return (
-      <>
-        <AnimatedTitle>Recommendations</AnimatedTitle>
-        <Table.Skeleton />
-      </>
-    );
-  }
-  if (data?.tracks == null) {
-    return (
-      <>
-        <AnimatedTitle>Recommendations</AnimatedTitle>
+  return (
+    <>
+      <AnimatedTitle>Recommendations</AnimatedTitle>
+      {isLoading ? <Table.Skeleton /> : null}
+      {data?.tracks == null ? (
         <NothingToShow
           message={'추천 트랙이 없습니다😢'}
           redirect={{ text: '검색하러 가기', path: PAGE.SEARCH }}
         />
-      </>
-    );
-  }
-  return (
-    <>
-      <AnimatedTitle>Recommendations</AnimatedTitle>
-      <Table items={data.tracks} />
+      ) : (
+        <Table items={data.tracks} />
+      )}
     </>
   );
 }
