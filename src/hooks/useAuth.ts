2d106b11df1d2f25fea93b24225f3c6c4fdb9979
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-10-16 14:33:29 +0900

    chore: clean up commented out code

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,53 +1,3 @@
-// import { ACCESS_TOKEN, SPOTIFY_WEB_API, VINYLIFY_TOKEN } from '@/constants';
-// import { API, PAGE } from '@/constants/url';
-// import { useCallback, useEffect, useMemo } from 'react';
-// import { useNavigate, useSearchParams } from 'react-router-dom';
-
-// export const useAuth = () => {
-//   const [searchParams] = useSearchParams();
-//   const navigate = useNavigate();
-//   useEffect(() => {
-//     const data = localStorage.getItem(VINYLIFY_TOKEN);
-//     if (
-//       data == null &&
-//       location.pathname == PAGE.LOGGED_IN &&
-//       searchParams.has(ACCESS_TOKEN)
-//     ) {
-//       const access_token = `${searchParams.get(ACCESS_TOKEN)}`;
-//       localStorage.setItem(VINYLIFY_TOKEN, access_token);
-//       SPOTIFY_WEB_API.setAccessToken(access_token);
-//       // navigate(PAGE.MAIN);
-//     }
-//   }, []);
-//   const validToken = useMemo(() => {
-//     if (!SPOTIFY_WEB_API.getAccessToken()) {
-//       if (
-//         location.pathname == PAGE.LOGGED_IN &&
-//         searchParams.has(ACCESS_TOKEN)
-//       ) {
-//         const access_token = `${searchParams.get(ACCESS_TOKEN)}`;
-//         localStorage.setItem(VINYLIFY_TOKEN, access_token);
-//         SPOTIFY_WEB_API.setAccessToken(access_token);
-//         // navigate(PAGE.MAIN);
-//         return access_token;
-//       } else {
-//         window.location.replace(API.LOGIN);
-//       }
-//     } else {
-//       return SPOTIFY_WEB_API.getAccessToken();
-//     }
-//   }, [searchParams, navigate]);
-
-//   const logOut = useCallback(() => {
-//     if (SPOTIFY_WEB_API.getAccessToken()) {
-//       localStorage.removeItem(VINYLIFY_TOKEN);
-//       SPOTIFY_WEB_API.setAccessToken(null);
-//       navigate(PAGE.MAIN);
-//     }
-//   }, [navigate]);
-//   return { data: validToken, logOut };
-// };
-
 import { ACCESS_TOKEN, SPOTIFY_WEB_API, VINYLIFY_TOKEN } from '@/constants';
 import { API, PAGE } from '@/constants/url';
 import { useCallback, useMemo } from 'react';
@@ -65,20 +15,6 @@ export const useAuth = () => {
     window.location.replace(API.LOGIN);
   }
 
-  // useEffect(() => {
-  //   const data = localStorage.getItem(VINYLIFY_TOKEN);
-  //   if (
-  //     data == null &&
-  //     location.pathname == PAGE.LOGGED_IN &&
-  //     searchParams.has(ACCESS_TOKEN)
-  //   ) {
-  //     const access_token = `${searchParams.get(ACCESS_TOKEN)}`;
-  //     localStorage.setItem(VINYLIFY_TOKEN, access_token);
-  //     SPOTIFY_WEB_API.setAccessToken(access_token);
-  //     // navigate(PAGE.MAIN);
-  //   }
-  // }, []);
-
   const validToken = useMemo(() => {
     if (!SPOTIFY_WEB_API.getAccessToken()) {
       if (
@@ -88,10 +24,7 @@ export const useAuth = () => {
         const access_token = `${searchParams.get(ACCESS_TOKEN)}`;
         localStorage.setItem(VINYLIFY_TOKEN, access_token);
         SPOTIFY_WEB_API.setAccessToken(access_token);
-        // navigate(PAGE.MAIN);
         return access_token;
-      } else {
-        // window.location.replace(API.LOGIN);
       }
     } else {
       if (
@@ -118,13 +51,8 @@ export const useAuth = () => {
     if (isFetching || window == null) {
       return;
     }
-    // const redirectUri = encodeURIComponent(window.location.href);
-    // window.location.replace(
-    //   `https://vinylify-tau.vercel.app/me?...&redirect_uri=${redirectUri}`,
-    // );
-    window.location.replace(API.LOGIN);
 
-    // https://vinylify-express.vercel.app/
+    window.location.replace(API.LOGIN);
   }, [window, data, isFetching]);
 
   const signUp = useCallback(() => {
