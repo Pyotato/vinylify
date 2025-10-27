f85169f0a9c08408ac030bd92ea226a62c37b8ee
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-10-27 16:04:20 +0900

    chore: add prod conditions

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,9 +1,11 @@
 export const API = {
   LOGIN: 'https://vinylify-express.vercel.app/',
   SPOTIFY: 'https://api.spotify.com/v1/',
-  RECCOBEATS: 'api/reccobeats',
-  GENIUS: 'api/genius',
-  LYRICS: 'api/lyrics',
+  RECCOBEATS: import.meta.env.PROD
+    ? 'https://api.reccobeats.com/v1'
+    : 'api/reccobeats',
+  GENIUS: import.meta.env.PROD ? 'https://api.genius.com' : 'api/genius',
+  LYRICS: import.meta.env.PROD ? 'https://api.lyrics.ovh/v1' : 'api/lyrics',
   EXPRESS: 'https://vinylify-express.vercel.app/',
 } as const;
 
