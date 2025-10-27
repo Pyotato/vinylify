32245f52f623e1a905d58fdaf9127b771c49bf09
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-10-27 12:15:42 +0900

    chore: remove files

--- src/components/Main/_shared/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function Layout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
-  return (
-    <section className="text-(--white) bg-(--grey-500) p-5">{children}</section>
-  );
-}
--- src/components/MusicInfo/ArtistInfo/ArtistName.tsx
+++
@@ -1,5 +0,0 @@
-const ArtistName = ({ artistName = '' }: { artistName?: string }) => {
-  return <span className={'text-(length:--text-fluid-xl)'}>{artistName} </span>;
-};
-
-export default ArtistName;
