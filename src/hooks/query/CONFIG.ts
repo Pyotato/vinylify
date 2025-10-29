2a3625047190234575442f0beb5d8be64c846d0a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-10-29 20:22:14 +0900

    chore: provide key as string

--- src/hooks/query/CONFIG.ts
+++ src/hooks/query/CONFIG.ts
@@ -2,22 +2,22 @@ import ERROR_MESSAGES from '@/config/ERROR_MESSAGES';
 
 export const retry = (failureCount: number, error: Error) => {
   if (
-    error?.message == ERROR_MESSAGES[401] ||
-    error?.message == ERROR_MESSAGES[403] ||
-    error?.message == ERROR_MESSAGES[429]
+    error?.message == ERROR_MESSAGES['401'] ||
+    error?.message == ERROR_MESSAGES['403'] ||
+    error?.message == ERROR_MESSAGES['429']
   ) {
     return false;
   }
 
-  if (failureCount < 3 || error?.message == ERROR_MESSAGES[408]) {
+  if (failureCount < 3 || error?.message == ERROR_MESSAGES['408']) {
     return true;
   } else return false;
 };
 
 export const throwOnError = (error: Error) => {
   return (
-    error?.message == ERROR_MESSAGES[401] ||
-    error?.message == ERROR_MESSAGES[403]
+    error?.message == ERROR_MESSAGES['401'] ||
+    error?.message == ERROR_MESSAGES['403']
   );
 };
 
